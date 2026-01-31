5dfeedcdbd93292a97d4ff902090913d62aa6eac
Athulsn10 <athulnair10@outlook.com>
2026-02-01 01:10:11 +0530

    feat: Add main CLI entry point with `init` command and update package version.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
 import { initCommand } from './commands/init.js';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+const packageJson = require('../package.json');
 
 const program = new Command();
 
 program
     .name('athul')
     .description('CLI tool for setting up WordPress projects locally using DDEV')
-    .version('1.0.0');
+    .version(packageJson.version);
 
 program
     .command('init')
